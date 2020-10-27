deae75510e85ee2e0a8f97d84dd558f6d6202048
Steven Gibson <devgibsonsp@gmail.com>
2020-10-27 00:46:06 -0400

    player item models show up, started process of syncing with server

--- Assets/Scripts/Character/UI/Inventory/playerInventory.cs
+++ Assets/Scripts/Character/UI/Inventory/playerInventory.cs
@@ -7,8 +7,10 @@ using ObjectData.ItemData.Utilities;
 using ObjectData.ItemData.Models;
 using UI;
 using CharacterNS;
+using Photon.Pun;
 
-public class playerInventory : MonoBehaviour 
+public class playerInventory : MonoBehaviourPunCallbacks, IPunObservable
+
 {
 
 	// *** Leaving a note here for future that if there is a split second that items aren't
@@ -28,13 +30,38 @@ public class playerInventory : MonoBehaviour
 
 	private bool CharacterLoaded { get; set; } = false;
 
-	private GameObject HeadRef {get; set; }
 
+
+
+
+	private GameObject BodyRef { get; set; }
+
+	private GameObject HeadRef { get; set; }
+	private GameObject RightHandRef { get; set; }
+	private GameObject LeftHandRef { get; set; }
+
+	private GameObject WeaponRef { get; set; }
+	private GameObject ShieldRef { get; set; }
+	private GameObject ArmorRef { get; set; }
+	private GameObject HelmRef { get; set; }
+
+
+	string EqHelm { get; set; }
+
+	string armor;
+
+	string EqWep { get; set; }
+
+	string EqShield { get; set; }
+
+
+	//private PhotonView PhotonView { get; set; }
 
 	// Use this for initialization
 	void Awake () 
 	{
-		
+		//PhotonView = PhotonView.Get(this);
+
 		StatsIndicator = GameObject.Find("StatsIndicator").GetComponent<Text>();
 
 		// Initializing the item lookup table
@@ -55,13 +82,85 @@ public class playerInventory : MonoBehaviour
 			FingerSlot3 = GameObject.Find("Finger3Slot"),
 			FingerSlot4 = GameObject.Find("Finger4Slot")
 		};
-		//HeadRef = new GameObject();
-		//HeadRef = CharacterObject.Ref.transform.Find("Head").gameObject;
-		
-		//GameObject.Find("PlayerV11(Clone)").transform.Find("Head").gameObject;
+
 		
 		
 	}
+
+
+   public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+   {
+       if (stream.IsWriting)
+       {
+           // We own this player: send the others our data
+
+		   if(WeaponRef != null)
+		   {
+			   stream.SendNext(WeaponRef.name);
+		   }
+		   else
+		   {
+			    stream.SendNext("No Weapon");
+		   }
+
+		   if(ShieldRef != null)
+		   {
+			   stream.SendNext(ShieldRef.name);
+		   }
+		   else
+		   {
+			    stream.SendNext("No Shield");
+		   }
+
+		   if(HelmRef != null)
+		   {
+			   stream.SendNext(HelmRef.name);
+		   }
+		   else
+		   {
+			    stream.SendNext("No Helm");
+		   }
+           
+			//stream.SendNext(ShieldRef.name);
+			//stream.SendNext(ArmorRef.name);
+			//stream.SendNext(HelmRef.name);
+           //stream.SendNext(Health);
+       }
+       else
+       {
+			string tWep   = (string)stream.ReceiveNext();
+			string tHelm   = (string)stream.ReceiveNext();
+			string tSheild = (string)stream.ReceiveNext();
+			Debug.Log(tWep   );
+			Debug.Log(tHelm  );
+			Debug.Log(tSheild);
+
+		   //// Network player, receive data
+		   //if(!EqWep.Equals(tWep))
+		   //{
+			//   SetEquipmentModels();
+			//   // do something
+		   //}
+		   //if(!EqWep.Equals(tHelm))
+		   //{
+			//   SetEquipmentModels();
+			//   // do something
+		   //}
+//
+		   //if(!EqWep.Equals(tSheild))
+		   //{
+			//   SetEquipmentModels();
+			//   // do something
+		   //}
+
+
+
+          
+       }    
+
+   }
+
+
 
 	void Update()
 	{
@@ -69,8 +168,11 @@ public class playerInventory : MonoBehaviour
 		if(CharacterObject.RefSet && !CharacterLoaded)
 		{
 			CharacterLoaded = true;
-			HeadRef = CharacterObject.Ref;//.transform.Find("Head").gameObject;
 			HeadRef = CharacterObject.Ref.transform.Find("PlayerBody/Bip001/Bip001 Pelvis/Bip001 Spine/Bip001 Neck/Bip001 Head/Bip001 HeadNub").gameObject;
+			BodyRef = CharacterObject.Ref.transform.Find("PlayerBody/Body").gameObject;
+			RightHandRef = CharacterObject.Ref.transform.Find("PlayerBody/Bip001/Bip001 Pelvis/Bip001 Spine/Bip001 R Clavicle/Bip001 R UpperArm/Bip001 R Forearm/Bip001 R Hand").gameObject;
+			LeftHandRef = CharacterObject.Ref.transform.Find("PlayerBody/Bip001/Bip001 Pelvis/Bip001 Spine/Bip001 L Clavicle/Bip001 L UpperArm/Bip001 L Forearm/Bip001 L Hand").gameObject;
+			SetEquipmentModels();
 		}
 
 		// If the item master list is initialized, flip the flag and calculate items
@@ -88,11 +190,17 @@ public class playerInventory : MonoBehaviour
 		if(IsPerformingEquipmentUpdate && !UserInterfaceLock.IsDragging)
 		{
 			IsPerformingEquipmentUpdate = false;
+
 			CalculateEquipment();
-			//SetEquipmentModels();
+			//PhotonView.RPC("SetEquipmentModels", RpcTarget.All);
+			
+			
+			SetEquipmentModels();
 		}
+		
 	}
 
+	// *** One thing to always note, this is simply for asthetics, this is not where armor calculations go ***
 	private void SetEquipmentModels()
 	{
 		// Whenever equipment is moved, detect the models on the player
@@ -101,11 +209,72 @@ public class playerInventory : MonoBehaviour
 		// Detect Helmet Changes
 		if(inventorySlots.HeadSlot.transform.childCount > 0)
 		{
+			if(HelmRef != null && !HelmRef.name.Equals(inventorySlots.HeadSlot.transform.GetChild(0).name))
+			{
+				HelmRef.transform.GetComponent<MeshRenderer>().enabled = false;
+			}
+
 			// reference to the helmet
-			HeadRef.transform.Find(inventorySlots.HeadSlot.transform.GetChild(0).name).GetComponent<MeshRenderer>().enabled = true;
-			//inventorySlots.HeadSlot.transform.FindChild()
-			//inventorySlots.HeadSlot.transform.GetChild(0).name;
-			Debug.Log("HeadSlot");
+			HelmRef = HeadRef.transform.Find(inventorySlots.HeadSlot.transform.GetChild(0).name).gameObject;
+			HelmRef.transform.GetComponent<MeshRenderer>().enabled = true;
+		}
+		else if(HelmRef != null)
+		{
+			HelmRef.transform.GetComponent<MeshRenderer>().enabled = false;
+			HelmRef = null;
+		}
+
+		if(inventorySlots.BodySlot.transform.childCount > 0)
+		{
+			if(ArmorRef != null && !BodyRef.name.Equals(inventorySlots.BodySlot.transform.GetChild(0).name))
+			{
+				ArmorRef.SetActive(false);
+			}
+			BodyRef.transform.Find("Body None").gameObject.SetActive(false);
+			ArmorRef = BodyRef.transform.Find(inventorySlots.BodySlot.transform.GetChild(0).name).gameObject;
+			ArmorRef.SetActive(true);
+		}
+		else if(ArmorRef != null)
+		{
+			BodyRef.transform.Find("Body None").gameObject.SetActive(true);
+			ArmorRef.SetActive(false);
+			ArmorRef = null;
+		}
+
+		if(inventorySlots.HandSlot1.transform.childCount > 0)
+		{
+			if(WeaponRef != null && !WeaponRef.name.Equals(inventorySlots.HandSlot1.transform.GetChild(0).name))
+			{
+				WeaponRef.transform.GetComponent<MeshRenderer>().enabled = false;
+			}
+
+			// reference to the helmet
+			WeaponRef = RightHandRef.transform.Find(inventorySlots.HandSlot1.transform.GetChild(0).name).gameObject;
+			WeaponRef.transform.GetComponent<MeshRenderer>().enabled = true;
+
+		}
+		else if(WeaponRef != null)
+		{
+			WeaponRef.transform.GetComponent<MeshRenderer>().enabled = false;
+			WeaponRef = null;
+		}
+
+		if(inventorySlots.HandSlot2.transform.childCount > 0)
+		{
+			if(ShieldRef != null && !ShieldRef.name.Equals(inventorySlots.HandSlot2.transform.GetChild(0).name))
+			{
+				ShieldRef.transform.GetComponent<MeshRenderer>().enabled = false;
+			}
+
+			// reference to the helmet
+			ShieldRef = LeftHandRef.transform.Find(inventorySlots.HandSlot2.transform.GetChild(0).name).gameObject;
+			ShieldRef.transform.GetComponent<MeshRenderer>().enabled = true;
+
+		}
+		else if(ShieldRef != null)
+		{
+			ShieldRef.transform.GetComponent<MeshRenderer>().enabled = false;
+			ShieldRef = null;
 		}
 
 	}
